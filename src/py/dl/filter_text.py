f5cc78993823ab2ae60c55fabc1e0bf37609244c
Juan Carlos Prieto <juanprietob@gmail.com>
2018-08-24 09:49:23 -0400

    BUG: Fix coordinate to capture text for image type

--- src/py/dl/filter_text.py
+++ src/py/dl/filter_text.py
@@ -12,8 +12,9 @@ def main(args):
 			if text["bounds"] and text["bounds"][0] and text["bounds"][0]["y"]:
 				y = text["bounds"][0]["y"]
 				x = text["bounds"][0]["x"]
-				
-				if 405 < y and y < 420 and x > 80:
+				# {"x": 54, "y": 417}, {"x": 105, "y": 417}, {"x": 105, "y": 431}
+				# [{"x": 87, "y": 418}, {"x": 109, "y": 418}, {"x": 109, "y": 429}, {"x": 87, "y": 429}]
+				if 405 < y and y < 420 and x > args.minx:
 					text_description.append(text["description"])
 
 		if len(text_description) > 1:
@@ -24,6 +25,9 @@ if __name__ == "__main__":
 	parser = argparse.ArgumentParser()
 	group = parser.add_mutually_exclusive_group(required=True)
 	group.add_argument('--json', type=str, help='JSON filename produced by detect_text.py')
+	
+	parser.add_argument('--minx', type=int, help='x coordinate min', default=40)
+	parser.add_argument('--join', type=str, help='join character', default=',')
 
 	args = parser.parse_args()
 
