604a86369152d0558a54a2540631091a49125691
juanprietob <juanprietob@gmail.com>
2018-10-12 11:27:28 -0400

    ENH: Option to set image dimension

--- src/py/rgb2lum.py
+++ src/py/rgb2lum.py
@@ -4,7 +4,7 @@ import os
 
 def main(args):
 	
-	InputType = itk.Image[itk.RGBPixel[itk.UC], 3]
+	InputType = itk.Image[itk.RGBPixel[itk.UC], args.dim]
 	print("Reading:", args.img)
 	reader = itk.ImageFileReader[InputType].New(FileName=args.img)
 	img = reader.GetOutput()
@@ -21,6 +21,7 @@ def main(args):
 if __name__ == "__main__":
 	parser = argparse.ArgumentParser()
 	parser.add_argument('--img', type=str, help='Input jpg image', required=True)
+	parser.add_argument('--dim', type=int, help='Dimension', default=3)
 	parser.add_argument('--out', type=str, help='Output filename', default="out.nrrd")
 
 	args = parser.parse_args()
