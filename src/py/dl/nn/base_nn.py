f129cca78f254659eeb5b4b23314980cf0ca3800
Juan Carlos Prieto <juanprietob@gmail.com>
2020-04-28 21:47:27 -0400

    ENH: Base clase for nn v1.15

--- src/py/dl/nn/base_nn.py
+++ src/py/dl/nn/base_nn.py
@@ -20,6 +20,7 @@ class BaseNN:
         self.num_channels = 1
         self.out_channels = 1
         self.global_step = 0
+        self.number_of_tfRecords = 0
 
     def set_global_step(self, step):
         self.global_step = step
@@ -42,6 +43,7 @@ class BaseNN:
         else:
             print("Nothing to decode! data_keys missing in object description object. tfRecords.py creates this descriptor.")
             raise
+
     def get_data_description(self):
         return self.data_description
     
@@ -68,6 +70,8 @@ class BaseNN:
         if((1, 15, 0) <= tuple([int(num) for num in tf.__version__.split('.')])):
             tf.data.experimental.ignore_errors()
 
+        self.number_of_tfRecords = len(tfrecords_arr)
+
         dataset = tf.data.TFRecordDataset(tfrecords_arr)
         dataset = dataset.map(self.read_and_decode)
         dataset = dataset.shuffle(buffer_size=buffer_size)
@@ -76,6 +80,9 @@ class BaseNN:
         iterator = dataset.make_initializable_iterator()
 
         return iterator
+
+    def get_number_of_tf_records(self):
+        return self.number_of_tfRecords
 
     def print_tensor_shape(self, tensor, string):
 
@@ -296,46 +303,122 @@ class BaseNN:
 
             return matmul_op
 
-    def conv_block(self, x0, in_filters, out_filters, block='a', is_training=False, ps_device="/cpu:0", w_device="/gpu:0"):
+    def conv_block(self, x0, in_filters, out_filters, block='a', activation=tf.nn.relu, is_training=False, initializer=tf.random_normal_initializer(mean=0,stddev=0.01), ps_device="/cpu:0", w_device="/gpu:0"):
         
-        x = self.convolution2d(x0, name= block + "_conv1_op", filter_shape=[1,1,in_filters,out_filters[0]], strides=[1,1,1,1], padding="SAME", use_bias=False, activation=None, ps_device=ps_device, w_device=w_device)
+        x = self.convolution2d(x0, name= block + "_conv1_op", filter_shape=[1,1,in_filters,out_filters[0]], strides=[1,1,1,1], padding="SAME", use_bias=False, activation=None, initializer=initializer, ps_device=ps_device, w_device=w_device)
         x = tf.layers.batch_normalization(x, training=is_training)
-        x = tf.nn.relu(x)
+        x = activation(x)
 
-        x = self.convolution2d(x, name=block + "_conv2_op", filter_shape=[3,3,out_filters[0],out_filters[1]], strides=[1,2,2,1], padding="SAME", use_bias=False, activation=None, ps_device=ps_device, w_device=w_device)
+        x = self.convolution2d(x, name=block + "_conv2_op", filter_shape=[3,3,out_filters[0],out_filters[1]], strides=[1,2,2,1], padding="SAME", use_bias=False, activation=None, initializer=initializer, ps_device=ps_device, w_device=w_device)
         x = tf.layers.batch_normalization(x, training=is_training)
-        x = tf.nn.relu(x)
+        x = activation(x)
 
-        x = self.convolution2d(x, name=block + "_conv3_op", filter_shape=[1,1,out_filters[1],out_filters[2]], strides=[1,1,1,1], padding="SAME", use_bias=False, activation=None, ps_device=ps_device, w_device=w_device)
+        x = self.convolution2d(x, name=block + "_conv3_op", filter_shape=[1,1,out_filters[1],out_filters[2]], strides=[1,1,1,1], padding="SAME", use_bias=False, activation=None, initializer=initializer, ps_device=ps_device, w_device=w_device)
         x = tf.layers.batch_normalization(x, training=is_training)
-        x = tf.nn.relu(x)     
+        x = activation(x)     
 
-        shortcut = self.convolution2d(x0, name=block + "_conv4_op", filter_shape=[1,1,in_filters,out_filters[2]], strides=[1,2,2,1], padding="SAME", use_bias=False, activation=None, ps_device=ps_device, w_device=w_device)    
+        shortcut = self.convolution2d(x0, name=block + "_conv4_op", filter_shape=[1,1,in_filters,out_filters[2]], strides=[1,2,2,1], padding="SAME", use_bias=False, activation=None, initializer=initializer, ps_device=ps_device, w_device=w_device)    
         shortcut = tf.layers.batch_normalization(shortcut, training=is_training)
 
         x = tf.math.add(x, shortcut)
-        x = tf.nn.relu(x)
+        x = activation(x)
 
         return x
 
-    def identity_block(self, x0, in_filters, out_filters, block='a', is_training=False, ps_device="/cpu:0", w_device="/gpu:0"):
+    def identity_block(self, x0, in_filters, out_filters, block='a', activation=tf.nn.relu, is_training=False, initializer=tf.random_normal_initializer(mean=0,stddev=0.01), ps_device="/cpu:0", w_device="/gpu:0"):
 
-        x = self.convolution2d(x0, name=block + "_conv1_op", filter_shape=[1,1,in_filters,out_filters[0]], strides=[1,1,1,1], padding="SAME", use_bias=False, activation=None, ps_device=ps_device, w_device=w_device)
+        x = self.convolution2d(x0, name=block + "_conv1_op", filter_shape=[1,1,in_filters,out_filters[0]], strides=[1,1,1,1], padding="SAME", use_bias=False, activation=None, initializer=initializer, ps_device=ps_device, w_device=w_device)
         x = tf.layers.batch_normalization(x, training=is_training)
-        x = tf.nn.relu(x)   
+        x = activation(x)   
 
-        x = self.convolution2d(x, name=block + "_conv2_op", filter_shape=[3,3,out_filters[0],out_filters[1]], strides=[1,1,1,1], padding="SAME", use_bias=False, activation=None, ps_device=ps_device, w_device=w_device)
+        x = self.convolution2d(x, name=block + "_conv2_op", filter_shape=[3,3,out_filters[0],out_filters[1]], strides=[1,1,1,1], padding="SAME", use_bias=False, activation=None, initializer=initializer, ps_device=ps_device, w_device=w_device)
         x = tf.layers.batch_normalization(x, training=is_training)
-        x = tf.nn.relu(x)  
+        x = activation(x)  
 
-        x = self.convolution2d(x, name=block + "_conv3_op", filter_shape=[1,1,out_filters[1],out_filters[2]], strides=[1,1,1,1], padding="SAME", use_bias=False, activation=None, ps_device=ps_device, w_device=w_device)
+        x = self.convolution2d(x, name=block + "_conv3_op", filter_shape=[1,1,out_filters[1],out_filters[2]], strides=[1,1,1,1], padding="SAME", use_bias=False, activation=None, initializer=initializer, ps_device=ps_device, w_device=w_device)
         x = tf.layers.batch_normalization(x, training=is_training)
-        x = tf.nn.relu(x) 
+        x = activation(x) 
         
         x = tf.math.add(x, x0)
-        x = tf.nn.relu(x)
+        x = activation(x)
 
         return x
+
+    def mutual_info_histo(self, hist2d):
+        # Get probability
+        pxy = tf.divide(hist2d, tf.reduce_sum(hist2d))
+        # marginal for x over y
+        px = tf.reduce_sum(pxy, axis=1)
+        # marginal for y over x
+        py = tf.reduce_sum(pxy, axis=0)
+
+        px_py = tf.multiply(px[:, None], py[None, :])
+
+        px_py = tf.boolean_mask(px_py, pxy)
+        pxy = tf.boolean_mask(pxy, pxy)
+
+        return tf.reduce_sum(tf.multiply(pxy, tf.math.log(tf.math.divide(pxy, px_py))))
+
+    def joint_entropy(self, hist2d):
+        # Get probability
+        pxy = tf.divide(hist2d, tf.reduce_sum(hist2d))
+        pxy = tf.boolean_mask(pxy, tf.math.greater(pxy, 0))
+
+        return -tf.reduce_sum(tf.multiply(pxy, tf.math.divide(tf.math.log(pxy), tf.math.log(2.0))))
+
+    def histogram(self, x, y, nbins=100, range_h=None):
+        
+        shape = tf.shape(y)
+        batch_size = shape[0]
+        
+        x = tf.reshape(x, [-1])
+        y = tf.reshape(y, [-1])
+        
+        if range_h is None:
+            range_h = [tf.reduce_min(tf.concat([x, y], axis=-1)), tf.reduce_max(tf.concat([x, y], axis=-1))]
+        
+        # hisy_bins is a Tensor holding the indices of the binned values whose shape matches y.
+        histy_bins = tf.histogram_fixed_width_bins(y, range_h, nbins=nbins, dtype=tf.int32)
+        # and creates a histogram_fixed_width 
+        H = tf.map_fn(lambda i: tf.histogram_fixed_width(tf.boolean_mask(x, tf.equal(histy_bins, i)), range_h, nbins=nbins, dtype=tf.int32), tf.range(nbins))
+        
+        return tf.cast(H, dtype=tf.float32)
+
+    def mutual_info(self, x, y, nbins=100):
+        return self.mutual_info_histo(self.histogram(x, y))
+
+    def mutual_info_channels(self, x, y, nbins=100):
+        xy = tf.stack([tf.unstack(x, axis=-1), tf.unstack(y, axis=-1)], axis=1)
+        return tf.reduce_sum(tf.map_fn(lambda xy_c: self.mutual_info(xy_c[0], xy_c[1]), xy, dtype=tf.float32))
+
+    def emd(self, x, y, nbins=100):
+
+        x = tf.reshape(x, [-1])
+        y = tf.reshape(y, [-1])
+
+        range_h = [tf.reduce_min(tf.concat([x, y], axis=-1)), tf.reduce_max(tf.concat([x, y], axis=-1))]
+
+        histo_x = tf.cast(tf.histogram_fixed_width(x, range_h, nbins=nbins, dtype=tf.int32), dtype=tf.float32)
+        histo_y = tf.cast(tf.histogram_fixed_width(y, range_h, nbins=nbins, dtype=tf.int32), dtype=tf.float32)
+
+        all_sorted_xy = tf.sort(tf.concat([histo_x, histo_y], axis=-1))
+        
+        all_sorted_xy_delta = tf.cast(all_sorted_xy[1:] - all_sorted_xy[:-1], dtype=tf.float32)
+
+        histo_x_sorted = tf.sort(histo_x)
+        histo_y_sorted = tf.sort(histo_y)
+
+        histo_x_indices = tf.searchsorted(histo_x_sorted, all_sorted_xy[:-1], side='right')
+        histo_y_indices = tf.searchsorted(histo_y_sorted, all_sorted_xy[:-1], side='right')
+
+        cmdf_x = tf.cast(tf.math.divide(histo_x_indices, nbins), dtype=tf.float32)
+        cmdf_y = tf.cast(tf.math.divide(histo_y_indices, nbins), dtype=tf.float32)
+        
+        return tf.math.sqrt(tf.reduce_sum(tf.math.multiply(tf.math.squared_difference(cmdf_x, cmdf_y), all_sorted_xy_delta)))
+
+    def emd_channels(x, y, nbins=100):
+        xy = tf.cast(tf.stack([tf.unstack(x, axis=-1), tf.unstack(y, axis=-1)], axis=1), dtype=tf.float32)
+        return tf.reduce_sum(tf.map_fn(lambda xy_c: self.emd(xy_c[0], xy_c[1], nbins), xy))
         
     def inference(self, data_tuple=None, images=None, keep_prob=1, is_training=False, ps_device="/cpu:0", w_device="/gpu:0"):
         raise NotImplementedError
@@ -350,5 +433,8 @@ class BaseNN:
     def prediction_type(self):
         print("Implement in your own class, the return type must be one of 'image', 'segmentation', 'class', 'scalar'", file=sys.stderr)
         raise NotImplementedError
-    def restore_variables(self):
-        return None
+    def restore_variables(self, restore_all=True):
+        vars_train = tf.trainable_variables()
+        if(restore_all):
+            return vars_train
+        return vars_train
